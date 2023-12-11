14579ab0b06e3ce15be696025e43bbc5fccd0ee7
Max Steffen <max.steffen@mail.com>
2023-12-11 10:52:58 +0100

    solve task 1

--- lib/task1.dart
+++ lib/task1.dart
@@ -5,13 +5,28 @@ void main(List<String> arguments) {
   // Read two values from user input and print the sum of them
 
   print('Enter first number:');
-  stdin.readLineSync();
+  final firstNumber = stdin.readLineSync() ?? '';
 
   // int.parse();
   // int.tryParse();
 
   print('Enter second number:');
-  stdin.readLineSync();
+  final secondNumber = stdin.readLineSync() ?? '';
+
+  final firstParsedNumber = int.tryParse(firstNumber);
+  final secondParsedNumber = int.tryParse(secondNumber);
+
+  if (firstParsedNumber == null || secondParsedNumber == null) {
+    print('Invalid input');
+    return;
+  }
+
+  final calculatedValue = calculate(
+    leftHand: firstParsedNumber,
+    rightHand: secondParsedNumber,
+  );
+
+  print('The sum of $firstParsedNumber and $secondParsedNumber is $calculatedValue');
 }
 
 int calculate({required int leftHand, required int rightHand}) {
